687ca04bb4eb5d0f619f528de4c4c4897da620d0
León Agustín Vila <leonvila98@gmail.com>
2021-05-14 18:07:21 -0300

    carousel planes hecho

---
+++ client/src/components/InfoPlans/InfoCard.jsx
@@ -0,0 +1,153 @@
+/* import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import { teal } from '@material-ui/core/colors';
+import { NavLink } from 'react-router-dom';
+import styles from './InfoCard.module.css'
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    avatar: {
+        backgroundColor: teal ['A100'],
+    },
+}));
+export default function InfoCard(props) {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root, styles.root}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label='plans' className={classes.avatar}>
+                        P
+                    </Avatar>
+                }
+                title={props.plan.description}
+                subheader={props.plan.price}
+            />
+            <NavLink
+                path
+                to={`planDetails/${props.plan.id_plan}`}
+                style={{ textDecoration: 'none', color: 'black' }}
+            >
+                Ver mas
+            </NavLink>
+            <CardContent>
+                <Typography paragraph>Cobertura:</Typography>
+                <Typography paragraph>
+                    {props.plan.benefits.benefits_title}
+                </Typography>
+                {props.plan.benefits.map((d, index) => (
+                    <Typography key={`cardInfo-${index}`} paragraph>
+                        {d.benefit_description}
+                    </Typography>
+                ))}
+            </CardContent>
+        </Card>
+    );
+} */
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import styles from './InfoCard.module.css';
+import logoNav from '../../assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
+
+// const useStyles = makeStyles({
+//     root: {
+//         maxWidth: 345,
+//     },
+    // media: {
+    //     height: 140,
+    // },
+// });
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+    //   backgroundColor: theme.palette.background.paper,
+      position: 'relative',
+      overflow: 'auto',
+    //   maxHeight: 300,
+    },
+    listSection: {
+      backgroundColor: 'inherit',
+    },
+    media: {
+        height: 140,
+    },
+    ul: {
+      backgroundColor: 'inherit',
+      padding: 0,
+    },
+  }));
+
+export default function InfoCard(props) {
+    const{plan}=props;
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={(classes.media, styles.picture)}
+                    image={logoNav}
+                    title='Logo'
+                />
+                <CardContent>
+                    <Typography
+                        className={styles.title}
+                        gutterBottom
+                        variant='h5'
+                    >
+                        {plan.name}
+                    </Typography>
+                    <Typography
+                        variant='body2'
+                        color='textSecondary'
+                        component='p'
+                    >
+                        ${plan.price}
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                <List className={classes.root} subheader={<li />}>
+                    <ul className={classes.ul}>
+                        {plan.benefits.map((d, index) => (
+                            <ListItem key={`cardInfo-${index}`}>
+                                <ListItemText primary={d.title} />
+                            </ListItem>
+                        ))}
+                    </ul>
+                </List>
+                    
+                    {/* {plan.benefits.map((d, index) => (
+                    <li key={`cardInfo-${index}`} paragraph>
+                        {d.title}
+                    </li>))} */}
+                </CardContent>
+                
+            </CardActionArea>
+            <CardActions>
+                <NavLink to={`planDetails/${props.plan.id_plan}`}>
+                    <Button size='small' color='primary'>
+                        Mas información
+                    </Button>
+                </NavLink>
+            </CardActions>
+        </Card>
+    );
+}
--- client/src/components/InfoPlans/InfoPlanes.jsx
+++ client/src/components/InfoPlans/InfoPlanes.jsx
@@ -1,71 +1,97 @@
-import React, { useState, useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getPlans } from '../../actions/getter.action'
-import MediaCard from './MediaCard';
+import React, { useState, useEffect } from 'react';
+import InfoCard from './InfoCard';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import supabase from '../../supabase.config';
+import * as Styles from './InfoPlans.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { getPlans } from '../../actions/getter.action';
 import { teal } from '@material-ui/core/colors';
 
-import * as styles from './InfoPlans.module.css';
 function InfoPlanes() {
-    // const [plans, setPlans] = useState([]);
-    const plans = useSelector((state) => state.allPlans);    
+    const plans = useSelector((state) => state.allPlans);
     const dispatch = useDispatch();
-    // useEffect(() => {
-    //     const fetchPlans = async () => {
-    //         let { data: plans } = await supabase.from('plans').select('*');
-    //         setPlans(plans);
-    //     };
-    //     fetchPlans();
-    // }, []);
-    useEffect(() => {
-        dispatch(getPlans())
-    }, []);
 
     const [currCard, setCurrCard] = useState(0);
     const [shouldAutoSlide,setAutoSlide] = useState(true);
-
-    if (plans.length === 0) return <h1>Cargando...</h1>;
 
     const leftPlan = plans[currCard > 1 ? currCard - 1 : plans.length - 1];
     const plan = plans[currCard];
     const rightPlan = plans[currCard < plans.length - 1 ? currCard + 1 : 0];
 
-    const forward = () =>{
+    const forward = () => {
         if(shouldAutoSlide){
             setAutoSlide(false);
         }
-        currCard < plans.length - 1 ? setCurrCard(currCard + 1) : setCurrCard(0);
-    }
-    const back = () =>{
+        currCard < plans.length - 1
+            ? setCurrCard(currCard + 1)
+            : setCurrCard(0);
+    };
+
+    const back = () => {
         if(shouldAutoSlide){
             setAutoSlide(false);
         }
-        currCard > 0 ? setCurrCard(currCard - 1) : setCurrCard(plans.length - 1);
-    }
-    
+        currCard > 0
+            ? setCurrCard(currCard - 1)
+            : setCurrCard(plans.length - 1);
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 39) {
+            forward();
+        }
+        if (e.keyCode === 37) {
+            back();
+        }
+    };
+
     const autoSlide=()=>{
-        currCard < plans.length - 1 ? setCurrCard(currCard + 1) : setCurrCard(0);
+        currCard < plans.length - 1
+            ? setCurrCard(currCard + 1)
+            : setCurrCard(0);
     }
 
     setTimeout(()=>{
         if(shouldAutoSlide){
             autoSlide()
         }
-    },2000);
+    },2500);
+
+    useEffect(() => {
+        dispatch(getPlans());
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    if (plans.length === 0) return <h2>Loading...</h2>;
 
     return (
-        <div className={styles.carousel}>
-            <div className={styles.left} onClick={back}>
+        <div className={Styles.carousel}>
+            <div className={Styles.left} onClick={back}>
                 <ArrowBackIosIcon style={{ color: teal[300] }} />
             </div>
-            <div className={styles.card}>
-                <MediaCard plan={leftPlan} />
-                <MediaCard plan={plan} />
-                <MediaCard plan={rightPlan} />
+            <div className={Styles.card}>
+                <InfoCard
+                    key={leftPlan.id_plan}
+                    className={Styles.center}
+                    plan={leftPlan}
+                />
+                <InfoCard
+                    key={plan.id_plan}
+                    className={Styles.center}
+                    plan={plan}
+                />
+                <InfoCard
+                    key={rightPlan.id_plan}
+                    className={Styles.center}
+                    plan={rightPlan}
+                />
             </div>
-            <div className={styles.right} onClick={forward}>
+            <div className={Styles.right} onClick={forward}>
                 <ArrowForwardIosIcon style={{ color: teal[300] }} />
             </div>
         </div>
